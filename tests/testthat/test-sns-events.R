17b6e6a7c87e2626ab4092f4ec109bc9ca8b77eb
mdneuzerling <david@neuzerling.com>
2021-10-24 08:43:53 +1100

    Include test for non-JSON SNS messages

--- tests/testthat/test-sns-events.R
+++ tests/testthat/test-sns-events.R
@@ -17,36 +17,38 @@ test_that("SNS events are detected", {
   )
 })
 
-raw_sns_event_content <- '{
-    "Records": [
-        {
-            "EventSource": "aws:sns",
-            "EventVersion": "1.0",
-            "EventSubscriptionArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
-            "Sns": {
-                "Type": "Notification",
-                "MessageId": "123456abcdef",
-                "TopicArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test",
-                "Subject": "INSUFFICIENT_DATA: \\"test-lambda-parity\\" in Asia Pacific (Sydney)",
-                "Message": "{\\"AlarmName\\":\\"test-lambda-parity\\",\\"AlarmDescription\\":null,\\"AWSAccountId\\":\\"1234567890\\",\\"NewStateValue\\":\\"INSUFFICIENT_DATA\\",\\"NewStateReason\\":\\"Insufficient Data: 1 datapoint was unknown.\\",\\"StateChangeTime\\":\\"2021-10-21T10:20:50.932+0000\\",\\"Region\\":\\"Asia Pacific (Sydney)\\",\\"AlarmArn\\":\\"arn:aws:cloudwatch:ap-southeast-2:1234567890:alarm:test-lambda-parity\\",\\"OldStateValue\\":\\"ALARM\\",\\"Trigger\\":{\\"MetricName\\":\\"NumberOfObjects\\",\\"Namespace\\":\\"AWS/S3\\",\\"StatisticType\\":\\"Statistic\\",\\"Statistic\\":\\"MAXIMUM\\",\\"Unit\\":null,\\"Dimensions\\":[{\\"value\\":\\"random-data\\",\\"name\\":\\"BucketName\\"},{\\"value\\":\\"AllStorageTypes\\",\\"name\\":\\"StorageType\\"}],\\"Period\\":60,\\"EvaluationPeriods\\":1,\\"ComparisonOperator\\":\\"GreaterThanOrEqualToThreshold\\",\\"Threshold\\":0.0,\\"TreatMissingData\\":\\"- TreatMissingData:                    missing\\",\\"EvaluateLowSampleCountPercentile\\":\\"\\"}}",
-                "Timestamp": "2021-10-21T10:20:50.973Z",
-                "SignatureVersion": "1",
-                "Signature": "abcdefghijklmnopqrstubwxyz",
-                "SigningCertUrl": "https://sns.ap-southeast-2.amazonaws.com/SimpleNotificationService-123456abcdef.pem",
-                "UnsubscribeUrl": "https://sns.ap-southeast-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
-                "MessageAttributes": {}
-            }
-        }
-    ]
-}'
-
-sns_event <- structure(
-  list(event_content = raw_sns_event_content),
-  class = c("sns_event", "event")
-)
-
 test_that("JSON SNS messages can be parsed", {
+
+  raw_sns_event_content <- '{
+      "Records": [
+          {
+              "EventSource": "aws:sns",
+              "EventVersion": "1.0",
+              "EventSubscriptionArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
+              "Sns": {
+                  "Type": "Notification",
+                  "MessageId": "123456abcdef",
+                  "TopicArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test",
+                  "Subject": "INSUFFICIENT_DATA: \\"test-lambda-parity\\" in Asia Pacific (Sydney)",
+                  "Message": "{\\"AlarmName\\":\\"test-lambda-parity\\",\\"AlarmDescription\\":null,\\"AWSAccountId\\":\\"1234567890\\",\\"NewStateValue\\":\\"INSUFFICIENT_DATA\\",\\"NewStateReason\\":\\"Insufficient Data: 1 datapoint was unknown.\\",\\"StateChangeTime\\":\\"2021-10-21T10:20:50.932+0000\\",\\"Region\\":\\"Asia Pacific (Sydney)\\",\\"AlarmArn\\":\\"arn:aws:cloudwatch:ap-southeast-2:1234567890:alarm:test-lambda-parity\\",\\"OldStateValue\\":\\"ALARM\\",\\"Trigger\\":{\\"MetricName\\":\\"NumberOfObjects\\",\\"Namespace\\":\\"AWS/S3\\",\\"StatisticType\\":\\"Statistic\\",\\"Statistic\\":\\"MAXIMUM\\",\\"Unit\\":null,\\"Dimensions\\":[{\\"value\\":\\"random-data\\",\\"name\\":\\"BucketName\\"},{\\"value\\":\\"AllStorageTypes\\",\\"name\\":\\"StorageType\\"}],\\"Period\\":60,\\"EvaluationPeriods\\":1,\\"ComparisonOperator\\":\\"GreaterThanOrEqualToThreshold\\",\\"Threshold\\":0.0,\\"TreatMissingData\\":\\"- TreatMissingData:                    missing\\",\\"EvaluateLowSampleCountPercentile\\":\\"\\"}}",
+                  "Timestamp": "2021-10-21T10:20:50.973Z",
+                  "SignatureVersion": "1",
+                  "Signature": "abcdefghijklmnopqrstubwxyz",
+                  "SigningCertUrl": "https://sns.ap-southeast-2.amazonaws.com/SimpleNotificationService-123456abcdef.pem",
+                  "UnsubscribeUrl": "https://sns.ap-southeast-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
+                  "MessageAttributes": {}
+              }
+          }
+      ]
+  }'
+
+  sns_event <- structure(
+    list(event_content = raw_sns_event_content),
+    class = c("sns_event", "event")
+  )
+
   parsed_content <- parse_event_content.sns_event(sns_event)
+  expect_true(is.list(parsed_content))
   expect_equal(
     names(parsed_content),
     c(
@@ -63,5 +65,36 @@ test_that("JSON SNS messages can be parsed", {
 
 })
 
+test_that("non-JSON SNS messages can be parsed", {
+  raw_sns_event_content <- '{
+      "Records": [
+          {
+              "EventSource": "aws:sns",
+              "EventVersion": "1.0",
+              "EventSubscriptionArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
+              "Sns": {
+                  "Type": "Notification",
+                  "MessageId": "123456abcdef",
+                  "TopicArn": "arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test",
+                  "Subject": "INSUFFICIENT_DATA: \\"test-lambda-parity\\" in Asia Pacific (Sydney)",
+                  "Message": "This is not JSON",
+                  "Timestamp": "2021-10-21T10:20:50.973Z",
+                  "SignatureVersion": "1",
+                  "Signature": "abcdefghijklmnopqrstubwxyz",
+                  "SigningCertUrl": "https://sns.ap-southeast-2.amazonaws.com/SimpleNotificationService-123456abcdef.pem",
+                  "UnsubscribeUrl": "https://sns.ap-southeast-2.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:ap-southeast-2:1234567890:lambda-parity-test:123456abcdef",
+                  "MessageAttributes": {}
+              }
+          }
+      ]
+  }'
 
+  sns_event <- structure(
+    list(event_content = raw_sns_event_content),
+    class = c("sns_event", "event")
+  )
 
+  parsed_content <- parse_event_content.sns_event(sns_event)
+  expect_false(is.list(parsed_content))
+  expect_equal(parsed_content, "This is not JSON")
+})
